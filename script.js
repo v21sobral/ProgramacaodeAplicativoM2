30c6e5be54557186c855efff593d57384a8f42fc
21Sobral <v21sobral@gmail.com>
2025-08-10 00:36:03 -0300

    css criado

--- script.js
+++ script.js
@@ -6,24 +6,23 @@ function gerarBotao() {
     resultadoDiv.innerHTML = ''; // Limpa o conteúdo anterior
 
     if (isNaN(idade) || idade < 0) {
-        resultadoDiv.innerHTML = 'Por favor, digite uma idade válida.';
+        resultadoDiv.innerHTML = `
+        <p class="idade-invalida">Por favor, digite uma idade válida.</p>
+        `;
         return;
     }
 
     if (idade >= 18) {
         resultadoDiv.innerHTML = `
-            <p style="color: green;">Você tem ${idade} anos.</p>
-            <p style="color: green;">Você é maior de idade e pode votar!</p>
+            <p class="pode-votar">Você tem ${idade} anos é maior de idade e pode votar!</p>
         `;
     } else if (idade >= 16) {
         resultadoDiv.innerHTML = `
-            <p style="color: blue;">Você tem ${idade} anos.</p>
-            <p style="color: blue;">Você pode votar, mas o voto é facultativo.</p>
+            <p class="voto-facultativo">Você tem ${idade} anos e pode votar, mas o voto é facultativo.</p>
         `;
     } else {
         resultadoDiv.innerHTML = `
-            <p style="color: red;">Você tem ${idade} anos.</p>
-            <p style="color: red;">Você não pode votar ainda.</p>
+            <p class="nao-pode-votar">Você tem ${idade} anos e não pode votar ainda.</p>
         `;
     }
 }
