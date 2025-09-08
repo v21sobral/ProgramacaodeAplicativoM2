54ae04b9b7fce84610a065cc2724033aef86a04a
21Sobral <v21sobral@gmail.com>
2025-09-07 23:45:09 -0300

    made in home

---
+++ deksa/projeto-page/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="Container">
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
