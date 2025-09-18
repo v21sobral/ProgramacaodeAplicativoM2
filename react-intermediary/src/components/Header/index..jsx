84860c5bd70303f02ca5d6b1acffd3d065e283ff
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-18 15:23:24 -0300

    intermeduary-project

---
+++ react-intermediary/src/components/Header/index..jsx
@@ -0,0 +1,33 @@
+// src\components\Header\index..jsx
+
+import { NavLink, Link } from 'react-router-dom'
+import './style.css'
+import logo from '../../assets/Logo.png'
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header_wrap">
+        <Link to='/' className='brand'>
+          <img src={logo} alt="Logo da empresa" className='logo'/>
+          <span>Contabilidade é nóis</span>
+        </Link>
+
+        <nav className='nav'>
+          <NavLink to='/servicos' className={({ isActive }) => isActive ? 'link active' : 'link'}>
+            Serviços
+          </NavLink>
+          <NavLink to='/sobre-nos' className={({ isActive }) => isActive ? 'link active' : 'link'}>
+            Sobre Nós
+          </NavLink>
+          <NavLink to='/fale-conosco' className={({ isActive }) => isActive ? 'link active' : 'link'}>
+            Fale Conosco
+          </NavLink>
+        </nav>
+      </div> {/* Fechar a div header_wrap */}
+    </header>
+  );
+}
+
+export default Header
+    
