d964b2dcf78ac3ff1489245d895da58fb977e75c
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-22 15:47:29 -0300

    Pronto pro vercel

--- react-intermediary/src/pages/Servicos/index.jsx
+++ react-intermediary/src/pages/Servicos/index.jsx
@@ -1,12 +1,18 @@
 // src\pages\Servicos\index.jsx
 
 import './style.css'
+import hero from '../../assets/qrcode.png'; 
 
 function Servicos() {
     return (
         <section className='servicos'>
             <h1>Serviços de gestão de entregas</h1>
             <p>Comodidade e segurança na palma da mão</p>
+
+            <div className='qrcode'>
+                <img src="src\assets\qrcode.png" alt="" />
+                <h3>Contamos com a tecnologia de qrcode para facilitar o seu dia-a-dia dando mais segurança e rapidez nas suas entregas. A tecnologia de Qrcode já faz parte do nosso dia-a-dia e entegrada ao serviço de entregas tudo fica mais fácil.</h3>
+            </div>
 
             <div className='grid cols-3'>
                 <div className='card'>
